60d61d5488f84ddbd1c731b0f676fbbaeff32f10
basservice <venkatesh.anbalagan@cognizant.com>
2016-10-20 07:44:27 -0400

    CrashTest
    CrashTest

--- src/test/java/org/springframework/samples/petclinic/web/CrashControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/web/CrashControllerTests.java
@@ -17,7 +17,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /**
- * Test class for {@link CrashController}
+ * Test class for  {@link CrashController}
  *
  * @author Colin But
  */
